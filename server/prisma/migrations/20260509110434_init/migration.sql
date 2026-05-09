d89cc0cb4962989f558d78e29402a3f7bb0d8bac
nuraia <nuraianahrin@gmail.com>
2026-05-09 17:10:55 +0600

    Implement Prisma Schema

---
+++ server/prisma/migrations/20260509110434_init/migration.sql
@@ -0,0 +1,135 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'visitor',
+    "phone" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Authority" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "department" TEXT,
+
+    CONSTRAINT "Authority_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Zone" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "habitatType" TEXT NOT NULL,
+    "capacity" INTEGER NOT NULL,
+    "MapCoords" TEXT,
+    "authorityId" INTEGER NOT NULL,
+
+    CONSTRAINT "Zone_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Caregiver" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "shift" TEXT,
+    "authorityId" INTEGER NOT NULL,
+
+    CONSTRAINT "Caregiver_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FoodSupplier" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "address" TEXT,
+    "supplyType" TEXT,
+
+    CONSTRAINT "FoodSupplier_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Animal" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "species" TEXT NOT NULL,
+    "dob" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "healthStatus" TEXT NOT NULL,
+    "imageURL" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "zoneId" INTEGER NOT NULL,
+    "caregiverId" INTEGER NOT NULL,
+
+    CONSTRAINT "Animal_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FeedingSchedule" (
+    "id" SERIAL NOT NULL,
+    "foodItem" TEXT NOT NULL,
+    "quantity" TEXT NOT NULL,
+    "feedTime" TEXT NOT NULL,
+    "frequency" TEXT NOT NULL,
+    "animalId" INTEGER NOT NULL,
+    "supplierId" INTEGER NOT NULL,
+
+    CONSTRAINT "FeedingSchedule_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Ticket" (
+    "id" SERIAL NOT NULL,
+    "visitDate" TIMESTAMP(3) NOT NULL,
+    "type" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'active',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Ticket_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Authority_email_key" ON "Authority"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Caregiver_email_key" ON "Caregiver"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FoodSupplier_email_key" ON "FoodSupplier"("email");
+
+-- AddForeignKey
+ALTER TABLE "Zone" ADD CONSTRAINT "Zone_authorityId_fkey" FOREIGN KEY ("authorityId") REFERENCES "Authority"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Caregiver" ADD CONSTRAINT "Caregiver_authorityId_fkey" FOREIGN KEY ("authorityId") REFERENCES "Authority"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Animal" ADD CONSTRAINT "Animal_zoneId_fkey" FOREIGN KEY ("zoneId") REFERENCES "Zone"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Animal" ADD CONSTRAINT "Animal_caregiverId_fkey" FOREIGN KEY ("caregiverId") REFERENCES "Caregiver"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FeedingSchedule" ADD CONSTRAINT "FeedingSchedule_animalId_fkey" FOREIGN KEY ("animalId") REFERENCES "Animal"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FeedingSchedule" ADD CONSTRAINT "FeedingSchedule_supplierId_fkey" FOREIGN KEY ("supplierId") REFERENCES "FoodSupplier"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Ticket" ADD CONSTRAINT "Ticket_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
